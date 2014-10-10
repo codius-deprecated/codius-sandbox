ecdbbea08c1dde1d1748a022fd341f253856fbdf
Torrie Fischer <torrie@ripple.com>
2014-10-09 17:34:17 -0700

    Add more example code to host.cpp

--- src/host.cpp
+++ src/host.cpp
@@ -6,6 +6,7 @@
 #include <memory>
 #include <string.h>
 #include <signal.h>
+#include <asm/unistd.h>
 
 namespace syscall_names {
 #include "names.cpp"
@@ -15,15 +16,17 @@ class DummySandbox : public Sandbox {
   virtual long int handleSyscall(long int id) override {
     std::string syscallName = syscall_names::names[id];
     printf ("Sandboxed module ran syscall %s (%d)\n", syscallName.c_str(), id);
+    if (id == __NR_write)
+      return -1;
     return id;
   }
 
   virtual void handleIPC(const std::vector<char> &buf) override {
-    printf ("Got RPC Buf: %s", buf.data());
+    printf ("Got IPC buf: %s\n", buf.data());
   }
 
   virtual void handleSignal(int signal) override {
-    if (signal == SIGSEGV) {
+    if (signal == SIGSEGV || signal == SIGABRT) {
       launchDebugger();
     }
   }
