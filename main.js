f816460ac8f43db2eb0aa40fa2eaa746dfc4c701
Torrie Fischer <torrie@ripple.com>
2014-10-14 15:28:03 -0700

    Sandbox inherits EventEmitter

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-exports = require('./build/Release/obj.target/codius-sandbox.node');
+var Sandbox = require('./build/Release/obj.target/codius-sandbox.node').Sandbox;
+var events = require('events');
+
+inherits(Sandbox, events.EventEmitter);
+exports.Sandbox = Sandbox;
+
+function inherits(target, source) {
+  for (var k in source.prototype)
+    target.prototype[k] = source.prototype;
+}
