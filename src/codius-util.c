9eddfb4fe6fe97d4d055f08f7d32895acd04a86c
Torrie Fischer <torrie@ripple.com>
2014-10-15 15:12:28 -0700

    unused--

--- src/codius-util.c
+++ src/codius-util.c
@@ -9,9 +9,8 @@
    Return response_len or -1 for error. */
 int codius_sync_call(const char* request_buf, size_t request_len,
                      char **response_buf, size_t *response_len) {
-  size_t bytes_read;
+  ssize_t bytes_read;
   const int sync_fd = 3;
-  int resp_len;
 
   codius_rpc_header_t rpc_header;
   rpc_header.magic_bytes = CODIUS_MAGIC_BYTES;
@@ -25,8 +24,6 @@ int codius_sync_call(const char* request_buf, size_t request_len,
     return -1;
   }
 
-  raise (SIGUSR1);
-  
   bytes_read = read(sync_fd, &rpc_header, sizeof(rpc_header));
   if (bytes_read==-1 || rpc_header.magic_bytes!=CODIUS_MAGIC_BYTES) {
     printf("Error reading from fd %d\n", sync_fd);
