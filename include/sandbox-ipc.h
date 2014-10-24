ac1e75a6aa031d771d66ac0adbc4390a0173a24b
Torrie Fischer <torrie@ripple.com>
2014-10-24 15:28:04 -0700

    docs++

--- include/sandbox-ipc.h
+++ include/sandbox-ipc.h
@@ -11,29 +11,82 @@ class SandboxIPC;
 
 typedef void (*SandboxIPCCallback)(SandboxIPC& ipc, void* user_data);
 
-struct SandboxIPC {
+/**
+ * Class that binds a file descriptor inside the sandbox with an external
+ * handler
+ */
+class SandboxIPC {
+public:
+  /**
+   * Constructor
+   *
+   * @param _dupAs File descriptor that will be exposed within the sandbox
+   */
   SandboxIPC(int _dupAs);
   ~SandboxIPC();
 
   using Ptr = std::unique_ptr<SandboxIPC>;
 
+  /**
+   * calls dup2(child, dupAs), resulting in the descriptor referred to by @p dupAs
+   * now pointing to @p child.
+   */
   bool dup();
+
+  /**
+   * Attaches the parent side of the IPC channel to the libuv event loop
+   *
+   * @param loop A libuv event loop
+   */
   bool startPoll(uv_loop_t* loop);
+
+  /**
+   * Removes the parent side of the IPC channel from the libuv event loop
+   */
   bool stopPoll();
 
   uv_poll_t poll;
+
+  /**
+   * Parent side of the IPC pipe
+   */
   int parent;
+
+  /**
+   * Child side of the IPC pipe
+   */
   int child;
+
+  /**
+   * File descriptor that will be connected through the sandbox
+   */
   int dupAs;
+
+  /**
+   * Called when there is data ready to be read out of the sandbox
+   */
   virtual void onReadReady() = 0;
 
 private:
   static void cb_forward (uv_poll_t* req, int status, int events);
 };
 
-struct CallbackIPC : public SandboxIPC {
+/**
+ * An implementation of @p SandboxIPC that executes a callback when data is
+ * ready to be read out of the sandbox
+ */
+class CallbackIPC : public SandboxIPC {
+public:
   CallbackIPC (int dupAs);
   void onReadReady() override;
+
+  /**
+   * Sets the callback that will be excuted when data is ready to be read out of
+   * the sandbox
+   *
+   * @param cb Callback to run
+   * @param user_data Data to pass to the callback
+   */
   void setCallback(SandboxIPCCallback cb, void* user_data);
 
   using Ptr = std::unique_ptr<CallbackIPC>;
--- include/sandbox.h
+++ include/sandbox.h
@@ -9,44 +9,180 @@
 class SandboxPrivate;
 class SandboxIPC;
 
+//FIXME: This shouldn't be public API. It is only used for libuv
 struct SandboxWrap {
   SandboxPrivate* priv;
 };
 
+/**
+ * seccomp sandbox
+ */
 class Sandbox {
   public:
     Sandbox();
     ~Sandbox();
+
+    /**
+     * Spawns a binary inside this sandbox. Arguments are the same as for
+     * execv(3)
+     */
     void spawn(char** argv);
 
     using Word = unsigned long;
     using Address = Word;
 
+    /**
+     * Represents a syscall
+     */
     class SyscallCall {
       public:
         SyscallCall (Word _id) : id(_id) {}
         SyscallCall () : id(-1) {}
+
+        /**
+         * Syscall number
+         */
         Word id;
+
+        /**
+         * Arguments to the syscall
+         */
         Word args[6];
     };
 
+    /**
+     * Called when a syscall is called from within the sandbox. Return a
+     * different SyscallCall object to rewrite the call used
+     *
+     * @param call Call that was attempted
+     * @return Call that will be executed instead of @call
+     */
     virtual SyscallCall handleSyscall(const SyscallCall &call) = 0;
+
+    /**
+     * Called when an IPC request from within the sandbox is generated.
+     *
+     * @param IPC request
+     * @return Result of handling the IPC request
+     */
     virtual codius_result_t* handleIPC(codius_request_t*) = 0;
+
+    /**
+     * Called when the sandboxed child receives a signal.
+     *
+     * @param signal Signal that the child recieved
+     */
     virtual void handleSignal(int signal) = 0;
+    
+    /**
+     * Called when the sandboxed child has exited
+     * 
+     * @param status Exit status of the child
+     */
     virtual void handleExit(int status) = 0;
+
+    /**
+     * Map a sandbox-side file descriptor to an outside handler
+     * 
+     * @param ipc Pointer to a SandboxIPC object
+     */
     void addIPC(std::unique_ptr<SandboxIPC>&& ipc);
+
+    /**
+     * Read a word of the child process' memory
+     *
+     * @param addr Address to read
+     * @return Word at @p addr in child's memory
+     */
     Word peekData (Address addr);
+
+    /**
+     * Read a contiguous chunk of the child process' memory
+     *
+     * @param addr Address to start reading from
+     * @length Bytes to read
+     * @buf Buffer to write to
+     * @return @p true if successful, @p false otherwise. @P errno will be set
+     * upon failure.
+     */
     bool copyData (Address addr, size_t length, void* buf);
+
+    /**
+     * Read a contiguous chunk of the child process' memory, stopping at the
+     * first null byte.
+     *
+     * @param addr Address to write to
+     * @param maxLength Maximum length of string to read
+     * @param buf Buffer that the read string will be written to
+     * @return @p true if successful, @p false otherwise. @p errno will be set
+     * upon failure.
+     */
     bool copyString (Address addr, int maxLength, char* buf);
+
+    /**
+     * Write a single word to the child process' memory
+     * 
+     * @param addr Address to write to
+     * @param word Word to write
+     * @return @p true if successful, @p false otherwise. @p errno will be set
+     * on failure.
+     */
     bool pokeData (Address addr, Word word);
+
+    /**
+     * Write a chunk of data to the scratch buffer inside the child's memory,
+     * and return the address it was written to
+     *
+     * @param length Length of @p buf
+     * @param buf Data to write
+     * @return Address the data was written to, aligned up to the nearest word.
+     */
     Address writeScratch(size_t length, const char* buf);
+
+    /**
+     * Frees all used scratch memory for re-use
+     */
     void resetScratch();
+
+    /**
+     * Writes a chunk of data to the child process' memory
+     *
+     * @param addr Address to write to
+     * @param length Length of @p buf
+     * @param buf Data to write
+     * @return @p true if successful, @p false otherwise. @p errno will be set upon
+     * error.
+     */
     bool writeData (Address addr, size_t length, const char* buf);
+
+    /**
+     * Returns the address of the scratch buffer inside the child process
+     */
     Address getScratchAddress () const;
 
+    /**
+     * Returns the child's PID
+     */
     pid_t getChildPID() const;
+
+    /**
+     * Returns true after the child has called execve()
+     */
     bool enteredMain() const;
+
+    /**
+     * Releases the sandbox's hold on the child. \b WARNING: Once this function
+     * is called, the child process is only constrained by the limits put in
+     * place prior to spawn()
+     *
+     * @param signal - The signal to send to the child upon release. Use 0 for
+     * no signal.
+     */
     void releaseChild(int signal);
+
+    /**
+     * Kills the child process with SIGKILL
+     */
     void kill();
 
   private:
